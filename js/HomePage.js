e2ec65a6092ff51018152ac3fc9e6dc09d397cba
MariussGustins <mariuss.gustins@outlook.com>
2023-09-26 20:45:18 +0300

    HomePage finished next step database

---
+++ js/HomePage.js
@@ -0,0 +1,47 @@
+
+//Add button
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the button element
+    var add_btn = document.getElementById("add_btn");
+
+    // Add a click event listener to the button
+    add_btn.addEventListener("click", function() {
+        // Display a message in the console when the button is clicked
+        console.log("add button works ;) new css");
+    });
+});
+
+
+//Delete Button
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the button element
+    var delete_btn = document.getElementById("delete_btn");
+
+    // Add a click event listener to the button
+    delete_btn.addEventListener("click", function() {
+        // Display a message in the console when the button is clicked
+        console.log("delete button works ;) new css");
+    });
+
+//Search Button
+});document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the button element
+    var search_btn = document.getElementById("search_btn");
+
+    // Add a click event listener to the button
+    search_btn.addEventListener("click", function() {
+        // Display a message in the console when the button is clicked
+        console.log("search button works ;) new css");
+    });
+});
+//Scan Button
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the button element
+    var scan_btn = document.getElementById("scan_btn");
+
+    // Add a click event listener to the button
+    scan_btn.addEventListener("click", function() {
+        // Display a message in the console when the button is clicked
+        console.log("scan button works ;) new css");
+    });
+});
