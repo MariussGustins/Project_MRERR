c32d5891b1f0d1210b5a4f27a06e95a0a56d8778
MariussGustins <mariuss.gustins@outlook.com>
2023-09-28 00:22:53 +0300

    buttons

---
+++ js/AddItems.js
@@ -0,0 +1,11 @@
+//LogIn button
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the button element
+    var login = document.getElementById("login");
+
+    // Add a click event listener to the button
+    login.addEventListener("click", function() {
+        // Display a message in the console when the button is clicked
+        console.log("login button works ;) new css");
+    });
+});
