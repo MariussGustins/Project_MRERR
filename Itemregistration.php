2a0be4372a90c73a9da5a4b7850cb5f00140479b
ervin <ervinsyekabsons335@gmail.com>
2023-10-03 08:39:45 +0300

    ready for data base

--- Itemregistration.php
+++ Itemregistration.php
@@ -1,8 +1,10 @@
 <?php
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "project_mrderr";
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -10,22 +12,28 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$item = $_POST['item_name'];
-$barcode_seller = $_POST['barcode_seller'];
-$barcode_manufacter = $_POST['barcode_manufacter'];
-$delivery_date_time = $_POST['dalivery_date_time'];
-$delivery_contact_person = $_POST['dalivery_contact_person'];
-$amount = $_POST['amount'];
-$price = $_POST['price'];
-$serial_nr = $_POST['serual_nr'];
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $item = $_POST['item1'];
+    $barcode_seller = $_POST['barcode'];
+    $barcode_manufacturer = $_POST['barcode(manu)'];
+    $delivery_date_time = $_POST['dateandtime'];
+    $delivery_contact_person = $_POST['delivery-contact-person'];
+    $amount = $_POST['amount'];
+    $price = $_POST['price'];
+    $serial_nr = $_POST['Serial'];
 
-$sql = "INSERT INTO items (item, barcode_seller, barcode_manufacturer, delivery_date_time, delivery_contact_person, amount, price, serial_nr) VALUES ('$item', '$barcode_seller', '$barcode_manufacter', '$delivery_date_time', '$delivery_contact_person', $amount, $price, '$serial_nr')";
+    $sql = "INSERT INTO items (item, barcode_seller, barcode_manufacturer, delivery_date_time, delivery_contact_person, amount, price, serial_nr)
+            VALUES ('$item', '$barcode_seller', '$barcode_manufacturer', '$delivery_date_time', '$delivery_contact_person', $amount, $price, '$serial_nr')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Item registered successfully!";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    // Execute the SQL query
+    if ($conn->query($sql) === TRUE) {
+        echo "Item registered successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 
+// Close the database connection
 $conn->close();
 ?>
