4407fa854d4d77bb42624f72b09ba3f909d45156
MariussGustins <mariuss.gustins@outlook.com>
2023-10-04 09:13:26 +0300

    database and php

--- js/AddItems.js
+++ js/AddItems.js
@@ -1,12 +1,39 @@
 //LogIn button
 document.addEventListener("DOMContentLoaded", function() {
     // Get a reference to the button element
-    var login = document.getElementById("login");
+    var add = document.getElementById("add");
 
     // Add a click event listener to the button
-    login.addEventListener("click", function() {
+    add.addEventListener("click", function() {
         // Display a message in the console when the button is clicked
         console.log("login button works ;) new css");
     });
 });
+//Scan Button
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the button element
+    var barcode_btn = document.getElementById("barcode_btn");
 
+    // Add a click event listener to the button
+    barcode_btn.addEventListener("click", function() {
+        // Display a message in the console when the button is clicked
+        console.log("barcode button works ;) new css");
+    });
+    barcode_btn.addEventListener("click", function (){
+        window.location.href = "indexQR.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the button element
+    var qr_btn = document.getElementById("qr_btn");
+
+    // Add a click event listener to the button
+    qr_btn.addEventListener("click", function() {
+        // Display a message in the console when the button is clicked
+        console.log("qr button works ;) new css");
+    });
+    qr_btn.addEventListener("click", function (){
+        window.location.href = "indexQR.html";
+    });
+});
