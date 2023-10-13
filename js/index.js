1c78066112b32307564b888070e9efc09bd0cc05
MariussGustins <mariuss.gustins@outlook.com>
2023-10-13 14:42:12 +0300

    remove items done

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//Remove button
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the button element
+    var remove = document.getElementById("remove");
+
+    // Add a click event listener to the button
+    remove.addEventListener("click", function() {
+        // Display a message in the console when the button is clicked
+        console.log("remove button works ;) new css");
+    });
+});
--- php/index.php
+++ php/index.php
@@ -13,27 +13,56 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Handle form submission for item removal
+// Handle form submission
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the array keys exist before accessing them
-    if (isset($_POST["item_to_remove"])) {
-        $item_to_remove = $_POST["item_to_remove"];
+    if (isset($_POST["item_name"], $_POST["serial_nr"], $_POST["amount"])) {
+        $item_name = $_POST["item_name"];
+        $serial_nr = $_POST["serial_nr"];
+        $amount_removed = intval($_POST["amount"]);
 
-        // Remove the item from the database based on item name or barcode
-        $sql = "DELETE FROM items WHERE item_name = '$item_to_remove' OR barcode = '$item_to_remove'";
+        // Check if the item exists in the inventory
+        $check_inventory_sql = "SELECT * FROM items WHERE item_name = '$item_name' AND serial_nr = '$serial_nr'";
+        $inventory_result = $conn->query($check_inventory_sql);
 
-        if ($conn->query($sql) === TRUE) {
-            echo "Item removed successfully!";
+        if ($inventory_result && $inventory_result->num_rows > 0) {
+            // Item found in inventory
+            $row = $inventory_result->fetch_assoc();
+            $available_amount = intval($row['amount']);
+            $price = floatval($row['price']);
+            $total_price = $price * $amount_removed;
+
+            if ($amount_removed <= $available_amount) {
+                // Sufficient quantity available for removal
+                // Update the inventory
+                $new_amount = $available_amount - $amount_removed;
+                $update_sql = "UPDATE items SET amount = $new_amount WHERE item_name = '$item_name' AND serial_nr = '$serial_nr'";
+                $conn->query($update_sql);
+
+                // Insert removed item data into the removed_items table
+                $dateandtime = date('Y-m-d H:i:s');
+                $delivery_contact_person = $row['delivery_contact_person'];
+                $barcode_manufacturer = $row['barcode_manufacturer'];
+
+                $insert_removed_item_sql = "INSERT INTO removed_items (barcode, item_name, dateandtime, delivery_contact_person, amount, price, serial_nr, barcode_manufacturer, total_price)
+                    VALUES ('{$row['barcode']}', '$item_name', '$dateandtime', '$delivery_contact_person', $amount_removed, $price, '$serial_nr', '$barcode_manufacturer', $total_price)";
+
+                if ($conn->query($insert_removed_item_sql) === TRUE) {
+                    echo "Removed $amount_removed items of $item_name successfully! Total Price: $total_price";
+                } else {
+                    echo "Error inserting into removed_items: " . $conn->error;
+                }
+            } else {
+                echo "In inventory, only $available_amount left for $item_name.";
+            }
         } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            echo "Item $item_name with serial number $serial_nr not found in inventory.";
         }
     } else {
-        echo "Item to remove is missing.";
+        echo "One or more form fields are missing.";
     }
 }
 
 // Close the database connection
 $conn->close();
 ?>
-
-
