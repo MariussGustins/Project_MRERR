6167e77006ba184a701af83fba1e1064eed48d5b
MariussGustins <mariuss.gustins@outlook.com>
2023-10-17 09:27:56 +0300

    cleanup

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 //Remove button
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Get a reference to the button element
     var remove = document.getElementById("remove");
 
     // Add a click event listener to the button
-    remove.addEventListener("click", function() {
+    remove.addEventListener("click", function () {
         // Display a message in the console when the button is clicked
         console.log("remove button works ;) new css");
     });
