8d2b3ba5e86bf20e5c40b6ff75a8e96a554b0476
MariussGustins <mariuss.gustins@outlook.com>
2023-10-24 08:45:16 +0300

    loading

--- php/register.php
+++ php/register.php
@@ -1,3 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <style>
+        .loading-container {
+            display: none;
+            align-items: center;
+            justify-content: center;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.5);
+            z-index: 999;
+        }
+        .loading-spinner {
+            border: 4px solid rgba(255, 255, 255, 0.3);
+            border-top: 4px solid #3498db;
+            border-radius: 50%;
+            width: 50px;
+            height: 50px;
+            animation: spin 2s linear infinite;
+        }
+        .check {
+            display: none;
+            color: green;
+            font-size: 150px;
+            margin-top: 360px;
+            margin-left: 890px;
+        }
+
+        @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+        }
+    </style>
+</head>
+<body>
+
+<div class="loading-container">
+    <div class="loading-spinner"></div>
+    <div class="check">&#10003;</div>
+</div>
+
 <?php
 // Database connection settings
 $servername = "localhost";
@@ -27,7 +73,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             VALUES ('$first_name', '$last_name', '$email', '$username', '$password', $agree_terms)";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Registration successful!";
+        // Display the loading animation and then morph into a check mark after 3 seconds
+        echo "<script>
+            $('.loading-container').show();
+            setTimeout(function() {
+                $('.loading-spinner').hide();
+                $('.check').show();
+            }, 3000);
+        </script>";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
@@ -36,3 +89,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // Close the database connection
 $conn->close();
 ?>
+
+</body>
+</html>
